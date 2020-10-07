7ac868377bada3bc91748ccbd0230abc8b2d8ad3
kosiazom <kosiazom@gmail.com>
2020-10-07 17:17:09 -0400

    first commit

---
+++ app/controllers/api/v1/auths_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::AuthsController < ApplicationController
+    skip_before_action :logged_in? , only: [:create]
+
+    #login
+    def create 
+        # debugger
+        user = User.find_by(username: params[:username])
+       
+        if user && user.authenticate(params[:password])
+          render json: {username: user.username, id: user.id, token: encode_token({user_id: user.id})}, status: :created
+        else
+            render json: {error: 'invalid username or password!'}
+        end
+    end
+
+
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::UsersController < ApplicationController
+ skip_before_action :logged_in?, only:[:create]
+
+#register to the app
+def create
+
+    @user = User.create(user_params)
+
+    if @user.valid?
+        render json: @user, only:[:username, :password], status: :created
+    else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+    end
+end
+
+private
+def user_params
+    params.permit(:username, :password)
+end
+
+
+end
